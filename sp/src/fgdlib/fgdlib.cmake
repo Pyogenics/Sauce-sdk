8cae1228342b28789a38781dc95fdbdb1294f3bb
Pyogenics <py0g3n1cs@gmail.com>
2022-12-03 17:49:24 +0000

    Added more cmake sources definitions

---
+++ sp/src/fgdlib/fgdlib.cmake
@@ -0,0 +1,10 @@
+set(SOURCES_FGDLIB
+fgdlib/gamedata.cpp
+fgdlib/gdclass.cpp
+fgdlib/gdvar.cpp
+fgdlib/inputoutput.cpp
+fgdlib/wckeyvalues.cpp
+)
+
+set(HEADERS_FGDLIB
+)
---
+++ sp/src/raytrace/raytrace.cmake
@@ -0,0 +1,8 @@
+set(SOURCES_RAYTRACE
+raytrace/raytrace.cpp
+raytrace/trace2.cpp
+raytrace/trace3.cpp
+)
+
+set(HEADERS_RAYTRACE
+)
