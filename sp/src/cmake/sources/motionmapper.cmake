6d5f7afd04569263565b6c0e0a6be42d7e679b9b
Pyogenics <py0g3n1cs@gmail.com>
2022-12-04 23:43:45 +0000

    Fixed remnants from old build system file

--- sp/src/cmake/sources/motionmapper.cmake
+++ sp/src/cmake/sources/motionmapper.cmake
@@ -1,8 +1,8 @@
 set(SOURCES_MOTIONMAPPER
-$File   ${CMAKE_SOURCE_DIR}/utils/common/cmdlib.cpp
-$File   ${CMAKE_SOURCE_DIR}/public/filesystem_helpers.cpp
-$File   ${CMAKE_SOURCE_DIR}/public/filesystem_init.cpp
-$File   ${CMAKE_SOURCE_DIR}/utils/common/filesystem_tools.cpp
-$File   ${CMAKE_SOURCE_DIR}/utils/motionmapper/motionmapper.cpp
-$File   ${CMAKE_SOURCE_DIR}/utils/common/scriplib.cpp
+	${CMAKE_SOURCE_DIR}/utils/common/cmdlib.cpp
+	${CMAKE_SOURCE_DIR}/public/filesystem_helpers.cpp
+	${CMAKE_SOURCE_DIR}/public/filesystem_init.cpp
+	${CMAKE_SOURCE_DIR}/utils/common/filesystem_tools.cpp
+	${CMAKE_SOURCE_DIR}/utils/motionmapper/motionmapper.cpp
+	${CMAKE_SOURCE_DIR}/utils/common/scriplib.cpp
 )
