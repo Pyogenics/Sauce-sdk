0abe349fccfbf0a27748b6e50029ee1480fc69b9
Pyogenics <py0g3n1cs@gmail.com>
2022-12-24 00:08:36 +0000

    Removed reference to non existent file

---
+++ sp/src/cmake/sources/utils/vvis.cmake
@@ -0,0 +1,23 @@
+set(SOURCES_VVIS
+	#${CMAKE_SOURCE_DIR}/public/tier0/memoverride.cpp
+	${CMAKE_SOURCE_DIR}/utils/vvis/common/bsplib.cpp
+	${CMAKE_SOURCE_DIR}/utils/vvis/common/cmdlib.cpp
+	${CMAKE_SOURCE_DIR}/public/collisionutils.cpp
+	${CMAKE_SOURCE_DIR}/public/filesystem_helpers.cpp
+	${CMAKE_SOURCE_DIR}/utils/vvis/flow.cpp
+	${CMAKE_SOURCE_DIR}/public/loadcmdline.cpp
+	${CMAKE_SOURCE_DIR}/public/lumpfiles.cpp
+	${CMAKE_SOURCE_DIR}/utils/vvis/common/mpi_stats.cpp
+	${CMAKE_SOURCE_DIR}/utils/vvis/mpivis.cpp
+	${CMAKE_SOURCE_DIR}/utils/vvis/common/MySqlDatabase.cpp
+	${CMAKE_SOURCE_DIR}/utils/vvis/common/pacifier.cpp
+	${CMAKE_SOURCE_DIR}/public/scratchpad3d.cpp
+	${CMAKE_SOURCE_DIR}/utils/vvis/common/scratchpad_helpers.cpp
+	${CMAKE_SOURCE_DIR}/utils/vvis/common/scriplib.cpp
+	${CMAKE_SOURCE_DIR}/utils/vvis/common/threads.cpp
+	${CMAKE_SOURCE_DIR}/utils/vvis/common/tools_minidump.cpp
+	${CMAKE_SOURCE_DIR}/utils/vvis/common/vmpi_tools_shared.cpp
+	${CMAKE_SOURCE_DIR}/utils/vvis/vvis.cpp
+	${CMAKE_SOURCE_DIR}/utils/vvis/WaterDist.cpp
+	${CMAKE_SOURCE_DIR}/public/zip_utils.cpp
+)
